33e5d44375b7c1ae711308056241d41bd4b06ce2
github_username <email_address>
2024-01-07 20:41:47 +0530

    updated the files

---
+++ test1.py
@@ -0,0 +1,16 @@
+import json
+import os
+import json
+import traceback
+import pandas as pd
+from dotenv import load_dotenv
+from src.mcqgenerator.utils import read_file,get_table_data
+import streamlit as st
+from langchain.callbacks import get_openai_callback
+from src.mcqgenerator.app import generate_evaluate_chain
+
+#loading json file
+with open('C:/Users/Joejoyal/mcqgen/Response.json', 'r') as file:
+    RESPONSE_JSON = json.load(file)
+
+print(RESPONSE_JSON)
